0de0dcc79415854db285f757820bf4dc89455a86
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-03-04 19:49:07 +0100

    added build specs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+// Macros (crate level)
+#![no_std]  // Compile as native binary without libc runtime
+#![no_main] // Override main function
+
+// Imports
+use core::panic::PanicInfo;
+
+// HACK: Hacky panic impl
+// TODO: Add panic message and free stack variables 
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop { }
+}
+
+// Custom main function
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
