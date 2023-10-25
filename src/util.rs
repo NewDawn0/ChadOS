b8dcab04cfabc2fd832bb7898f52f6bbac48aa3a
NewDawn0 <aci.v.i.iv@gmail.com>
2023-10-25 19:00:22 +0200

    update init

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,6 @@
-use bootloader::BootInfo;
-
 use crate::kprintln;
+use bootloader::BootInfo;
+use const_format::str_replace;
 use core::arch::asm;
 pub unsafe fn hlt_loop() -> ! {
     loop {
@@ -8,18 +8,26 @@ pub unsafe fn hlt_loop() -> ! {
     }
 }
 
+macro_rules! kinit {
+    ($($module:ident)::*) => {{
+            kprintln!("Initializing ChadOS::{}::init",str_replace!(stringify!($($module)::*), " :: ", "::"));
+            $crate::$($module)::*::init()
+        }
+    };
+    ($($module:ident)::*, $($arg:expr),*) => {{
+            kprintln!("Initializing ChadOS::{}::init",str_replace!(stringify!($($module)::*), " :: ", "::"));
+            $crate::$($module)::*::init($($arg),*)
+        }
+    }
+}
+
 pub fn init(boot_info: &'static BootInfo) {
     kprintln!("Beginning initalization");
-    kprintln!("Initializing ChadOS::interrupt::gdt::init");
-    crate::interrupt::gdt::init();
-    kprintln!("Initializing ChadOS::interrupt::idt::init");
-    crate::interrupt::idt::init();
-    kprintln!("Initializing ChadOS::interrupt::pic::init");
-    crate::interrupt::pic::init();
-    kprintln!("Initializing ChadOS::mem::paging::init");
-    let (mut mapper, mut frame_alloc) = crate::mem::paging::init(boot_info);
-    kprintln!("Initializing ChadOS::mem::heap::init");
-    crate::mem::heap::init(&mut mapper, &mut frame_alloc).expect("Heap init failed");
+    kinit!(interrupt::gdt);
+    kinit!(interrupt::idt);
+    kinit!(interrupt::pic);
+    let (mut mapper, mut frame_alloc) = kinit!(mem::paging, boot_info);
+    kinit!(mem::heap, &mut mapper, &mut frame_alloc).expect("Heap init failed");
     kprintln!("Kernel fully initalized");
 }
 
