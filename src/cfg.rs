9a27d491c99b2c8e26e6c59a7dd13276599c3446
NewDawn0 <aci.v.i.iv@gmail.com>
2023-10-26 00:27:49 +0200

    add block sizes + rm unused

--- src/cfg.rs
+++ src/cfg.rs
@@ -6,14 +6,13 @@ pub mod vga {
     use crate::io::vga::Colour;
     pub const BUFFER_HEIGHT: usize = 25;
     pub const BUFFER_WIDTH: usize = 80;
-    pub const NON_PRINTABLE: u8 = 0xfe;
     pub const ASCII_BLANK: u8 = b' ';
     pub const BUF_ADDR: u32 = 0xb8000;
     pub const FG_COL: Colour = Colour::White;
     pub const BG_COL: Colour = Colour::Black;
-    pub const DEBUG_COLOUR: Colour = Colour::Yellow;
+    pub const WARN_COLOUR: Colour = Colour::Yellow;
     pub const ERRP_COLOUR: Colour = Colour::Red;
-    pub const ERRM_COLOUR: Colour = Colour::Red;
+    pub const ERRM_COLOUR: Colour = Colour::LightRed;
     pub const KERNELP_COLOUR: Colour = Colour::Green;
     pub const KERNELM_COLOUR: Colour = Colour::LightGreen;
 }
@@ -30,6 +29,8 @@ pub mod interrupt {
 pub mod mem {
     pub const HEAP_START: usize = 0x444444440000;
     pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
+    #[cfg(feature = "alloc-bump")]
+    pub const BLOCK_SIZES: &[usize] = &[8, 16, 32, 64, 128, 256, 512, 1024, 2048];
 }
 // Tests
 #[test_case]
@@ -44,10 +45,6 @@ fn test_cfg() {
         assert_eq!(vga::BUFFER_WIDTH, 80 as usize)
     );
     test!(
-        "CFG VGA NON_PRINTABLE",
-        assert_eq!(vga::NON_PRINTABLE, 0xfe as u8)
-    );
-    test!(
         "CFG VGA BUFFER_HEIGHT",
         assert_eq!(vga::ASCII_BLANK, b' ' as u8)
     );
@@ -55,10 +52,7 @@ fn test_cfg() {
         "CFG VGA BUFFER_HEIGHT",
         assert_eq!(vga::BUF_ADDR, 0xb8000 as u32)
     );
-    test!(
-        "CFG VGA FG_COL",
-        assert_eq!(vga::FG_COL, Colour::LightGreen)
-    );
+    test!("CFG VGA FG_COL", assert_eq!(vga::FG_COL, Colour::White));
     test!("CFG VGA BG_COL", assert_eq!(vga::BG_COL, Colour::Black));
     test!(
         "CFG SERIAL SERIAL1_PORT",
