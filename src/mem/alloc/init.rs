db126ae3936cd9ac0e01eb8a445f9be971382bd3
NewDawn0 <aci.v.i.iv@gmail.com>
2023-11-01 11:12:16 +0100

    rm old + add tests

--- src/mem/alloc/init.rs
+++ src/mem/alloc/init.rs
@@ -1,3 +1,8 @@
+use crate::cfg::mem::{HEAP_SIZE, HEAP_START};
+#[cfg(test)]
+use crate::test;
+#[cfg(feature = "alloc-bump")]
+use spin::{Mutex, MutexGuard};
 use x86_64::{
     structures::paging::{
         mapper::MapToError, FrameAllocator as FrameAlloc, Mapper, Page, PageTableFlags as Flags,
@@ -6,18 +11,10 @@ use x86_64::{
     VirtAddr,
 };
 
-use crate::cfg::mem::{HEAP_SIZE, HEAP_START};
-#[cfg(feature = "alloc-bump")]
-use spin::{Mutex, MutexGuard};
-
 #[cfg(feature = "alloc-bump")]
 use crate::mem::alloc::backend::bump::bump::ALLOC;
-#[cfg(feature = "alloc-dlmalloc")]
-use crate::mem::alloc::backend::dlmalloc::ALLOC;
 #[cfg(feature = "alloc-galloc")]
 use crate::mem::alloc::backend::galloc::ALLOC;
-#[cfg(feature = "alloc-slab")]
-use crate::mem::alloc::backend::slab::ALLOC;
 
 #[cfg(feature = "alloc-bump")]
 pub struct Locked<T> {
@@ -62,4 +59,30 @@ pub fn init(
         ALLOC.lock().init(HEAP_START, HEAP_SIZE);
     }
     Ok(())
+}
+
+#[test_case]
+fn test_alloc() {
+    use alloc::{boxed::Box, rc::Rc, vec::Vec};
+    let r#box = Box::new(911);
+    test!("MEM ALLOC INIT box heap malloc", assert_eq!(*r#box, 911));
+    let mut vec = Vec::new();
+    for i in 0..1000 {
+        vec.push(i);
+    }
+    test!(
+        "MEM ALLOC INIT large vec heap malloc",
+        assert_eq!(vec.iter().sum::<u64>(), (1000 - 1) * 1000 / 2)
+    );
+    let rc1 = Rc::new(5);
+    let rc2 = Rc::clone(&rc1);
+    test!(
+        "MEM ALLOC INIT large rc ref pt1 count heap malloc",
+        assert_eq!(Rc::strong_count(&rc1), 2)
+    );
+    core::mem::drop(rc2);
+    test!(
+        "MEM ALLOC INIT large rc ref pt2 count heap malloc",
+        assert_eq!(Rc::strong_count(&rc1), 1)
+    );
 }
