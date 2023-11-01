f91992db522ac70ddf8956bc1fc2fdbf2463d4d5
NewDawn0 <aci.v.i.iv@gmail.com>
2023-11-01 11:11:27 +0100

    remove old

--- src/mem/alloc/backend/mod.rs
+++ src/mem/alloc/backend/mod.rs
@@ -1,8 +1,4 @@
 #[cfg(feature = "alloc-bump")]
 pub mod bump;
-#[cfg(feature = "alloc-dlmalloc")]
-pub mod dlmalloc;
 #[cfg(feature = "alloc-galloc")]
 pub mod galloc;
-#[cfg(feature = "alloc-slab")]
-pub mod slab;
